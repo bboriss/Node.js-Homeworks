59ab2d7e17babbbb0244928e863fc242ea476ae7
Boris Stevanovic <96570966+bboriss@users.noreply.github.com>
2022-05-12 16:18:29 +0200

    Delete Homeework 10 directory

--- Homeework 10/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controlers/user");
-
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.delete("/:id", userController.deleteUserById);
-
-module.exports = router;
